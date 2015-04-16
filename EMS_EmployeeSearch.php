c37f5c0a113677e2a7c9b773f3d93f0a9d9c0902
wboldt-cc <wboldt-cc@conestogac.on.ca>
2015-04-16 19:14:44 -0400

    Style changes

--- EMS_EmployeeSearch.php
+++ EMS_EmployeeSearch.php
@@ -11,7 +11,7 @@ Date: December 8, 2013
 		<style>
 			body
 			{
-				background-color:#d0e4fe;
+				background-color:#585858;
 			}
 		</style>
 
@@ -19,13 +19,27 @@ Date: December 8, 2013
 
 		<title>EMS-PPS</title>
 
-		<div class="logo">
-			<img src="images\Logo.jpg" alt="Sorry, this image could not be displayed" width= "454" height="153"/>
-		</div>
+		
 
 		<?php
 			// Start the session
 			session_start();
+			
+			$lastNameToSearchFor = "";
+			$firstNameToSearchFor = "";
+			$SINtoSearchFor = "";
+			
+			$queryString = "";
+			
+			$serverName = $_SESSION['serverName'];
+			$userName = $_SESSION['userName'];
+			$password = $_SESSION['password'];
+			$databaseName = $_SESSION['databaseName'];	
+			$userType = $_SESSION['userType'];
+			
+			$link = "";
+			
+			
 		?>
 		
 	</head>
@@ -57,8 +71,89 @@ Date: December 8, 2013
 		<div class="content"> </br>
 		
 		
-			<h2>Search for an Employee</h2>
-			Blah blah?
+			<h2>Search Employees</h2>
+			<hr>
+			<form method='post'>
+			<?php
+			if(!empty($_POST['firstName']))
+			{
+				$firstNameToSearchFor = $_POST['firstName'];
+			}
+			else
+			{
+				$firstNameToSearchFor = "";
+			}
+			
+			if(!empty($_POST['lastName']))
+			{
+				$lastNameToSearchFor = $_POST['lastName'];
+			}
+			else
+			{
+				$lastNameToSearchFor = "";
+			}
+			
+			if(!empty($_POST['SIN']))
+			{
+				$SINtoSearchFor = $_POST['SIN'];
+			}
+			else
+			{
+				$SINtoSearchFor = "";
+			}
+			
+			
+			echo "
+							First Name: 
+							<input type='text' name='firstName' value='$firstNameToSearchFor'>
+							&nbsp &nbsp Last Name: 
+							<input type='text' name='lastName' value='$lastNameToSearchFor'></br></br>
+							Social Insurance Number: 
+							<input type='text' name='SIN' value='$SINtoSearchFor'>
+							</br></br>
+																			
+							<input type='submit' value='Search'><br><hr>
+						";
+			
+			if(($lastNameToSearchFor != "") || ($firstNameToSearchFor != "") || ($SINtoSearchFor != ""))// make sure at least one of the search criteria pieces is not blank
+			{
+				$link = mysqli_connect($serverName, $userName, $password, $databaseName); //connects to the database
+								
+				if(!$link)
+				{
+					//if the database connection failed send error message
+					 echo "<br>Error: Could not connect to the database. Please check your input.";
+				}
+				else// we have a connection
+				{
+					$dropdownMenu = constructDropdownMenu($lastNameToSearchFor, $firstNameToSearchFor, $SINtoSearchFor, $link);
+					
+					echo "$dropdownMenu";
+							
+					if(!empty($_POST['employeeToDisplay']))
+					{
+					$stringy = $_POST['employeeToDisplay'];
+						echo "test     ";
+						echo "$stringy";
+					}
+					
+					
+					
+					
+$link->close();
+								
+				}
+			}
+			
+			
+			
+			
+			
+			
+			
+			?>
+			
+			
 
 		</div>
 
@@ -68,6 +163,45 @@ Date: December 8, 2013
 		<div class="footer">
 			Copyright &copy Ava-Program-O!
 		</div>
+		
+		
+		<?php
+		
+			
+			function constructDropdownMenu($lastNameToSearchFor, $firstNameToSearchFor, $SINtoSearchFor, $link)
+			{
+				$returnString = "";
+			
+				//display lastname firstname and sin of employees found in list form.
+				//User will be able to click on them and display that employees info
+				$returnString = "
+									<select name='employeeToDisplay'><!--dropdown box which specifies the sorting method-->
+										<option value=''></option>
+										<option value='$SINtoSearchFor'>$lastNameToSearchFor, $firstNameToSearchFor, $SINtoSearchFor</option>
+										<option value='CompanyName'>Dirt, Joe, 333 333 334</option>
+										<option value='ContactName'>Contact Name</option>
+									</select>
+									
+									<input type='submit' value='Display'><br>
+									
+									
+								
+								";
+			
+				return $returnString;
+			}
+			
+			
+			function changeDisplayedEmployee()
+			{
+			
+			}
+			
+			
+		
+		?>
+		
+		</form>
 
 	</body>
 
