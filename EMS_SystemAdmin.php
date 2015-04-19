88b54c41e0c8ea0a702efedb4bb57f938c14332b
wboldt-cc <wboldt-cc@2A314-E08.conestogac.on.ca>
2015-04-19 14:30:29 -0400

    Can now add companies in System Admin

--- EMS_SystemAdmin.php
+++ EMS_SystemAdmin.php
@@ -27,6 +27,8 @@ Date: December 8, 2013
 			$password = $_SESSION['password'];
 			$databaseName = $_SESSION['databaseName'];	
 			$userType = $_SESSION['userType'];
+			
+			$link = "";
 		?>
 
 	</head>
@@ -39,8 +41,6 @@ Date: December 8, 2013
 			<h1>EMS-PPS</h1>
 			<br/>
 		</div>
-		
-		
 		
 
 		<div class="menu">
@@ -59,6 +59,63 @@ Date: December 8, 2013
 		<div class="content"> </br>
 			<h2>System Administration</h2>
 			
+			
+			<form method='post'>
+			
+			<?php
+			if($userType == 'administrator')
+			{						
+				echo "Enter the name of the company to add: ";
+				echo "<input type='text' name='companyToAdd'> ";
+				echo "&nbsp&nbsp<input type='submit' value='Add'><br><hr>";
+				
+				
+				if(!empty($_POST['companyToAdd']))
+				{
+					$companyToAdd = $_POST['companyToAdd'];
+					
+					if($companyToAdd != "")
+					{
+						if(strlen($companyToAdd) >= 50)// make sure the company name isn't too long
+						{
+							echo "The company name you entered was too long. We accept up to 50 characters. Please enter a shorter company name.";
+						}
+						else// company length is proper length
+						{
+						
+							$link = mysqli_connect($serverName, $userName, $password, $databaseName);// connect to the database
+										
+							if(!$link)
+							{
+								//if the database connection failed send error message
+								 echo "<br>Error: Could not connect to the database.";
+							}
+							else// we have a connection
+							{
+							
+								$returnedString = addCompany($companyToAdd, $link);
+							
+								echo "<br>$returnedString";
+								
+							}
+						}
+						
+					}// end 'if' statement
+					
+				}// end 'if' statement	
+				
+				
+			}
+			else// user is not an administrator
+			{
+				echo "You do not have access to this page.";
+			}
+			
+			
+			?>
+			
+			
+			</form>
 		</div>
 
 		<div class="margin">
@@ -67,6 +124,40 @@ Date: December 8, 2013
 		<div class="footer">
 			Copyright &copy MATTHEWSOFT
 		</div>
+		
+		<?php
+		// this is where all of the functions for the page are declared
+			
+			/*
+			 * Function: 
+			 * Description: 
+			 * Parameters: 
+			 * Return: 
+			 */
+			function addCompany($companyToAdd, $link)
+			{
+				$returnString = "";				
+				$queryString = "INSERT INTO Company (companyName) VALUES('$companyToAdd');";							
+				
+				if($result = $link->query($queryString))// make sure query was successful
+				{
+					
+					$returnString .= "The company: '$companyToAdd' was successfully added to the database.";
+					
+					//$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "There was an error attempting to enter the company into the database.";
+//$returnString = $queryString;
+				}	
+				
+				return $returnString;
+			}
+			
+			
+			
+		?>
 
 	</body>
 
