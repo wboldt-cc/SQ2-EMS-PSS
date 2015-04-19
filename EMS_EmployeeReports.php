80f18d1d232f6207652eaaaf8fca0e605dbcc07b
wboldt-cc <wboldt-cc@2A314-E08.conestogac.on.ca>
2015-04-19 15:16:49 -0400

    Stuff done with Reports UI

--- EMS_EmployeeReports.php
+++ EMS_EmployeeReports.php
@@ -21,7 +21,16 @@ Date: December 8, 2013
 
 		<?php
 			// Start the session
-			session_start();
+			session_start();			
+			
+			$serverName = $_SESSION['serverName'];
+			$userName = $_SESSION['userName'];
+			$password = $_SESSION['password'];
+			$databaseName = $_SESSION['databaseName'];	
+			$userType = $_SESSION['userType'];
+			
+			$link = "";			
+			
 		?>
 		
 	</head>
@@ -55,8 +64,21 @@ Date: December 8, 2013
 		<div class="content"> </br>
 			<h2>Employee Reports</h2>
 			<?php
-			echo "<form method='post'>
-						<input type='submit' value='Login'><br>
+			echo "<form method='post'>		
+					Which type of report would you like to generate? 
+						<select name='reportToGenerateDropDown'>
+										<option value=''></option>
+										<option value='sReport'>Seniority</option>
+										<option value='whwReport'>Weekly Hours Worked</option>";
+			if($userType == "administrator")
+			{
+				echo "<option value='pReport'>Payroll</option>
+					  <option value='aeReport'>Active Employee</option>
+					  <option value='ieReport'>Inactive Employee</option>";
+			}
+			
+			echo "		</select>
+						<input type='submit' value='Generate Report'><br><hr>										
 				  </form>";
 			?>
 
@@ -68,6 +90,250 @@ Date: December 8, 2013
 		<div class="footer">
 			Copyright &copy MATTHEWSOFT
 		</div>
+		
+		<?php
+		// this is where all of the functions for the page are declared
+			
+			/*
+			 * Function: 
+			 * Description: This function generates the ___ Report and returns it as a string
+			 * Parameters: The link to the database connection and the name of the company
+			 *             that the report should be generated for
+			 * Return: The ___ Report as a string
+			 */
+			function generate_sReport($link, $companyName)
+			{
+				$returnString = "Seniority Report ($companyName)<br>";
+				$queryString = "SELECT * FROM sReport WHERE companyName=$companyName;";
+			
+				if($result = $link->query($queryString))
+				{
+					/* add the headings for each column */
+					$returnString .= "<table border='1'>
+									  <tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+									  </tr>";
+					
+					while($row = $result->fetch_assoc())
+					{
+						$returnString .= "<tr>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+										  </tr>";
+													
+					}				
+					
+					$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "Could not generate the report. Sorry for the inconvenience";
+				}
+				
+				return $returnString;
+			}
+			
+			/*
+			 * Function: 
+			 * Description: This function generates the ___ Report and returns it as a string
+			 * Parameters: The link to the database connection and the name of the company
+			 *             that the report should be generated for
+			 * Return: The ___ Report as a string
+			 */
+			function generate_whwReport($link, $companyName)
+			{
+				$returnString = "";
+				$queryString = "";
+				
+				if($result = $link->query($queryString))
+				{
+					/* add the headings for each column */
+					$returnString .= "<table border='1'>
+									  <tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+									  </tr>";
+					
+					while($row = $result->fetch_assoc())
+					{
+						$returnString .= "<tr>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+										  </tr>";
+													
+					}				
+					
+					$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "Could not generate the report. Sorry for the inconvenience";
+				}
+				
+				return $returnString;
+			}
+			
+			/*
+			 * Function: 
+			 * Description: This function generates the ___ Report and returns it as a string
+			 * Parameters: The link to the database connection and the name of the company
+			 *             that the report should be generated for
+			 * Return: The ___ Report as a string
+			 */
+			function generate_pReport($link, $companyName)
+			{
+				$returnString = "";
+				$queryString = "";
+				
+				if($result = $link->query($queryString))
+				{
+					/* add the headings for each column */
+					$returnString .= "<table border='1'>
+									  <tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+									  </tr>";
+					
+					while($row = $result->fetch_assoc())
+					{
+						$returnString .= "<tr>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+										  </tr>";
+													
+					}				
+					
+					$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "Could not generate the report. Sorry for the inconvenience";
+				}
+				
+				return $returnString;
+			}
+			
+			/*
+			 * Function: 
+			 * Description: This function generates the ___ Report and returns it as a string
+			 * Parameters: The link to the database connection and the name of the company
+			 *             that the report should be generated for
+			 * Return: The ___ Report as a string
+			 */
+			function generate_aeReport($link, $companyName)
+			{
+				$returnString = "";
+				$queryString = "";
+				
+				if($result = $link->query($queryString))
+				{
+					/* add the headings for each column */
+					$returnString .= "<table border='1'>
+									  <tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+									  </tr>";
+					
+					while($row = $result->fetch_assoc())
+					{
+						$returnString .= "<tr>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+										  </tr>";
+													
+					}				
+					
+					$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "Could not generate the report. Sorry for the inconvenience";
+				}
+				
+				return $returnString;
+			}
+			
+			/*
+			 * Function: 
+			 * Description: This function generates the ___ Report and returns it as a string
+			 * Parameters: The link to the database connection and the name of the company
+			 *             that the report should be generated for
+			 * Return: The ___ Report as a string
+			 */
+			function generate_ieReport($link, $companyName)
+			{
+				$returnString = "";
+				$queryString = "";
+				
+				if($result = $link->query($queryString))
+				{
+					/* add the headings for each column */
+					$returnString .= "<table border='1'>
+									  <tr>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+										<th></th>
+									  </tr>";
+					
+					while($row = $result->fetch_assoc())
+					{
+						$returnString .= "<tr>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+											<td>" . $row[""] . "</td>
+										  </tr>";
+													
+					}				
+					
+					$result->free();
+				}
+				else// query failed
+				{
+					$returnString = "Could not generate the report. Sorry for the inconvenience";
+				}
+				
+				return $returnString;
+			}
+			
 
 	</body>
 
